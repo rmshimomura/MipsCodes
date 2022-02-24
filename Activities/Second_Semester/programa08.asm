1868da5861025841ed4718f44c1f96cd73eaee26
rmshimomura <shimomurarodrigo1@gmail.com>
2022-02-23 22:31:33 -0300

    Letter E done!

--- Activities/Second_Semester/programa08.asm
+++ Activities/Second_Semester/programa08.asm
@@ -1,5 +1,5 @@
 .data
-msgTamanho: .asciiz "Insira o tamanho do vetor:"
+msgTamanho: .asciiz "Insira o tamanho do vetor: "
 msgInserirValores1: .asciiz "Insira o valor (inteiro!) de Vet["
 msgInserirValores2: .asciiz "]:"
 msgZeroErro: .asciiz "O valor informado deve ser maior do que 0!\n\n"
@@ -9,6 +9,10 @@ msgNumeroIgual: .asciiz "Numero de elementos iguais a k: "
 msgValorK: .asciiz "Insira o valor K: "
 pulaLinha: .asciiz "\n"
 msgOrdenado: .asciiz "Vetor esta organizado!\n"
+msgFinal: .asciiz "Perfeitos - Semiprimos =  "
+
+soma_perfeitos: .word 0
+soma_semiprimos: .word 0
 
 .text
 
@@ -18,7 +22,10 @@ main:
 	jal escrita 
 	jal somarPares 			
 	jal maior_menor 		
-	jal iguais 			
+	jal iguais
+	jal perfect 			
+	jal semiPrime
+	jal showLastResult
 	
 	li $v0, 10
 	syscall
@@ -76,10 +83,11 @@ leitura_vetor:
 sort: 
 
 	beq $s7, 1, return_sort
-	
+	j loopFora
 	return_sort:
+		
 		li $v0, 4 			# Imprimir strings
-		la $a0, msgOrdenado 		# Mensagem de erro N <= 0
+		la $a0, msgOrdenado 		# Mensagem de vetor ordenado
 		syscall 
 		jr $ra
 
@@ -269,13 +277,20 @@ adicionar_igual:
 	syscall
 	jr $ra
 	
+perfect:
+
+	li $t1, 1 			# $t1 reset (Divisor atual)
+	li $t2, 0 			# $t2 reset (Contador Loop)
+	li $s4, 0 			# $s4 reset (soma dos divisores do numero atual)
+	move $s2, $s0 			# $s2 vai ser o runner
+			
 loopPerfeito:
 	
-	beq $s1, $t1, end
-	
-	div $t2, $t0, $t1
-	mfhi $k1
-	seq $t3, $k1, 0
+	lw $t4, ($s2) 			# Carrega em $t4, o valor do endereco $s2
+	beq $t1, $t4, end		
+	div $t9, $t4, $t1 		# $t9 = $t4/$t1 (Valor do vetor)/contador
+	mfhi $k1			# $k1 = $t4 % $t1
+	seq $t3, $k1, 0			# $t3 guarda se o numero em questao eh divisivel pelo contador
 	
 	beq $t3, 1, somarPerfeito
 	
@@ -285,10 +300,120 @@ loopPerfeito:
 
 somarPerfeito:
 	
-	add $s0, $s0, $t1
-	addi $t1, $t1, 1
+	add $s4, $s4, $t1		# Soma dos divisores += Divisor atual
+	addi $t1, $t1, 1		# Divisor atual++
 	j loopPerfeito
 	
 end:
-	li $v0, 10
+	seq $k1, $s4, $t4 		# Se a soma dos divisores for igual ao numero original, seta k1 para 1
+	beq $k1, 1, perfeito
+	beq $k1, 0, naoPerfeito
+
+perfeito:
+	
+	add $t6, $t6, $s4		# $t6 ira ser a soma dos numeros perfeitos
+	sw $t6, soma_perfeitos		# Salvar na "variavel"
+	add $s2, $s2, -4 		# Andar endereco
+	addi $t2, $t2, 1 		# Andar contador 
+	li $t1, 1 			# $t1 reset (Divisor atual)
+	li $s4, 0 			# $s4 reset (soma dos divisores)
+	blt $t2, $s7, loopPerfeito
+	jr $ra
+
+naoPerfeito:
+	
+	add $s2, $s2, -4 		# Andar endereco
+	addi $t2, $t2, 1 		# Andar contador 
+	li $t1, 1 			# $t1 reset (Divisor atual)
+	li $s4, 0 			# $s4 reset (soma dos divisores)
+	blt $t2, $s7, loopPerfeito
+	jr $ra	
+
+semiPrime:
+	
+	li $t0, 0 			# $t0 reset (cnt)
+	li $t1, 2			# $t1 reset (i)
+	li $t2, 0 			# $t2 reset (Contador Loop numero de elementos ja percorridos)
+	li $k1, 0			# $k1 reset (Soma dos semi primos)
+	move $s2, $s0 			# $s2 vai ser o runner
+	j loop1SemiPrime
+	
+loop1SemiPrime:
+	
+	lw $t4, ($s2) 			# Carrega em $t4, o valor do endereco $s2
+	add $a3, $zero, $t4		# Valor original
+	
+	wow:
+	sge $t3, $t0, 2			# cnt >= 2
+	mul $t5, $t1, $t1		# $t5 = i * i
+	sgt $t6, $t5, $t4 		# i * i > num
+	
+	beqz $t3, tryOther		# Se cnt < 2
+	
+	sle $t8, $t4, 1			# num <= 1 ? 
+	
+	beqz $t8, raiseCnt			# se num > 1 go to raiseCnt
+	
+	seq $t9, $t0, 2			# cnt == 2 ?
+	beq $t9, 1, raiseSemiPrimeCount # Se sim, adicione na soma dos semiprimos 
+	beqz $t9, continueLoop		# Se nao, continue o loop com o proximo elemento
+	
+	tryOther:
+		beqz $t6, loop2SemiPrime # Se i * i <= numero em analise
+		sle $t8, $t4, 1		 # num <= 1 ? 
+		beqz $t8, raiseCnt	 # se num > 1 go to raiseCnt
+		
+	j continueLoop
+		
+loop2SemiPrime:
+
+	div $t4, $t1			# num % i
+	mfhi $t7			# num % i
+	beqz $t7, prepareAgain		# num % i == 0 ?
+	add $t1, $t1, 1			# i++
+	j wow
+	
+prepareAgain:
+	
+	div $t4, $t4, $t1		# num /= i
+	add $t0, $t0, 1			# cnt++
+	j loop2SemiPrime
+
+raiseCnt:
+	
+	add $t0, $t0, 1			# cnt++
+	seq $t9, $t0, 2			# cnt == 2 ?
+	beq $t9, 1, raiseSemiPrimeCount # Se sim, adicione na soma dos semiprimos 
+	beqz $t9, continueLoop		# Se nao, continue o loop com o proximo elemento
+	
+raiseSemiPrimeCount:
+	 
+	add $k1, $k1, $a3
+	sw $k1, soma_semiprimos
+	
+	continueLoop:
+	
+		add $s2, $s2, -4 	# Andar endereco
+		addi $t2, $t2, 1 	# Andar contador
+		li $t0, 0 		# $t0 reset (cnt)
+		li $t1, 2		# $t1 reset (i)
+		blt $t2, $s7, loop1SemiPrime 
+		jr $ra
+		
+showLastResult:
+
+	li $t0, 0
+	lw $t1, soma_perfeitos
+	lw $t2, soma_semiprimos
+	
+	li $t3, 0
+	
+	sub $t3, $t1, $t2
+
+	la $a0, msgFinal
+	li $v0, 4
 	syscall
+	la $a0, ($t3)
+	li $v0, 1
+	syscall
+	jr $ra
