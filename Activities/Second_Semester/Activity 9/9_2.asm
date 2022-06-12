1aac84bd93bced5950fef95c1e4a11bd8a83cdb2
rmshimomura <shimomurarodrigo1@gmail.com>
2022-06-12 18:52:04 -0300

    9_2.asm done

---
+++ Activities/Second_Semester/Activity 9/9_2.asm
@@ -0,0 +1,62 @@
+.data
+str: .asciiz "Entre com k: "
+str1: .asciiz "Entre com n: "
+str2: .asciiz "K elevado a N é: "
+.text
+
+    	main:
+
+        	la $a0,str
+	        li $v0,4
+	        syscall
+	        li $v0,5
+        	syscall
+	        move $t0,$v0
+	        la $a0,str1
+	        li $v0,4
+	        syscall
+	        li $v0,5
+	        syscall
+	        move $a0, $t0    	# $a0 = X
+	        move $a1,$v0    	# $a1 = N    
+	
+	        jal power
+
+	        move $s0,$v0
+	        la $a0,str2
+	        li $v0,4
+	        syscall
+	        move $a0,$s0
+	        li $v0,1
+	        syscall
+	        li $v0,10
+		syscall
+	
+		jal power
+	
+	        power: 
+	        
+			bne $a1, $zero, recursion
+			li $v0, 1
+			jr $ra
+			
+		recursion:
+		
+			add $sp, $sp, -4
+			sw $ra, ($sp)
+			add $a1, $a1, -1
+			jal power
+			mul $v0, $a0, $v0
+			lw $ra, ($sp)
+			add $sp, $sp, 4
+			jr $ra
+		
+		move $s0,$v0
+        	la $a0,str2
+	        li $v0,4
+	        syscall
+	        move $a0,$s0
+	        li $v0,1
+	        syscall
+	        li $v0,10
+	        syscall	
